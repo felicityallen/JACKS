e6b00ab8b8131976cab255f162c471c351a2bba2
Felicity Allen <fa9@sanger.ac.uk>
2018-06-18 20:38:11 +0100

    Mirroring support for different controls per sample to run_JACKS_reference.py (fixing bug introduced with previous commit).

--- jacks/run_JACKS_reference.py
+++ jacks/run_JACKS_reference.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     LOG.setLevel(logging.WARNING)
     
     if len(sys.argv) != 6:
-        print('Usage: run_JACKS_reference.py countfile replicatefile:rep_hdr:sample_hdr:ctrl_sample guidemappingfile:sgrna_hdr:gene_hdr sgrna_reference_file outputprefix')
+        print('Usage: run_JACKS_reference.py countfile replicatefile:rep_hdr:sample_hdr:ctrl_sample_or_hdr guidemappingfile:sgrna_hdr:gene_hdr sgrna_reference_file outputprefix')
     else:
 
         #Parse arguments
@@ -24,7 +24,7 @@ if __name__ == '__main__':
         rep_toks = sys.argv[2].split(':')
         if len(rep_toks) != 4:
             raise Exception('Incorrect replicate file input: expecting "replicatefile:rep_hdr:sample_hdr:ctrl_sample" where replicatefile is a csv or tab delimited file mapping replicates to samples, rep_hdr and sample_hdr specify the column headers for the columns containing the replicate labels and sample labels respectively, and ctrl_sample specifies the name of the control sample')
-        replicatefile, rep_hdr, sample_hdr, ctrl_sample = rep_toks
+        replicatefile, rep_hdr, sample_hdr, ctrl_sample_or_hdr = rep_toks
         
         guide_toks = sys.argv[3].split(':')
         if len(guide_toks) != 3:
@@ -46,7 +46,7 @@ if __name__ == '__main__':
         
         #Load the specification of samples to include
         print('Loading sample specification')
-        sample_spec = createSampleSpec(countfile, replicatefile, rep_hdr, sample_hdr)
+        sample_spec, ctrl_per_sample, ctrl_spec = createSampleSpec(countfile, replicatefile, rep_hdr, sample_hdr, ctrl_sample_or_hdr)
         
         #Load the mappings from guides to genes
         print('Loading gene mappings')
@@ -69,13 +69,19 @@ if __name__ == '__main__':
             
         #Run all samples against the control
         print('Running JACKS inference')
-        ctrldata = data[:,sample_ids.index(ctrl_sample),:]
-        testdata = data[:,[i for i,x in enumerate(sample_ids) if ctrl_sample not in x],:]
+        if ctrl_per_sample:     #Different control samples specified per test sample
+            test_sample_idxs = [i for i,x in enumerate(sample_ids) if ctrl_spec[x] != x]
+            testdata = data[:,test_sample_idxs,:]
+            ctrldata = data[:,[sample_ids.index(ctrl_spec[sample_ids[idx]]) for idx in test_sample_idxs],:]
+        else:                   #Same control sample for all tests
+            ctrldata = data[:,sample_ids.index(ctrl_sample_or_hdr),:]
+            test_sample_idxs = [i for i,x in enumerate(sample_ids) if x != ctrl_sample_or_hdr]
+            testdata = data[:,test_sample_idxs,:]
         jacks_results = infer_JACKS(gene_index, testdata, ctrldata, fixed_x=x_reference)
 
         #Write out the results
         print('Writing JACKS results')
-        sample_ids_without_ctrl = [x for x in sample_ids if x != ctrl_sample]
+        sample_ids_without_ctrl = [sample_ids[idx] for idx in test_sample_idxs]
         writeJacksWResults( outfile_w, jacks_results, sample_ids_without_ctrl)
         writeJacksWResults( outfile_w2, jacks_results, sample_ids_without_ctrl, write_w2=True)
         writeJacksXResults( outfile_x, jacks_results, gene_grnas )
