e948cb1e5a2a5e400e98328c03ebae2746bd274b
Anton Khodak <anton.khodak@ukr.net>
2018-09-28 14:10:17 +0100

    Standardise command line arguments

--- jacks/run_JACKS.py
+++ jacks/run_JACKS.py
@@ -67,65 +67,62 @@ def loadSgrnaReference(filename):
     return x_ref
 
 
-run_jacks_doc = """
+def get_run_jacks_parser():
+    """
+    Create command-line parser for run_jacks program
+    """
+    run_jacks_doc = ""
+    ap = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                 description=run_jacks_doc)
+    ap.add_argument("countfile",
+                    help="Countfile: assumes sgrna label is always in the first column!")
+    replicatefile_group = ap.add_argument_group("Replicate file arguments")
+    replicatefile_group.add_argument("replicatefile",
+                                     help="A CSV or tab delimited file mapping replicates to samples")
+    replicatefile_group.add_argument("--rep-hdr",
+                                     type=str,
+                                     default="Replicate",
+                                     help="Column header for columns containing the replicate labels")
+    replicatefile_group.add_argument("--sample-hdr",
+                                     type=str,
+                                     default="Sample",
+                                     help="Column header for columns containing the sample labels")
+    replicatefile_group.add_argument("--ctrl-sample",
+                                     type=str,
+                                     default="CONTROL",
+                                     help="Name of the control sample")
+    guidemapping_group = ap.add_argument_group("Guidemapping file arguments")
+    guidemapping_group.add_argument("guidemappingfile",
+                                    help="A CSV or tab delimited file mapping guides to genes")
+    guidemapping_group.add_argument("--sgrna-hdr",
+                                    type=str,
+                                    default="Guide",
+                                    help="Column headers for the columns containing the guide labels")
+    guidemapping_group.add_argument("--gene-hdr",
+                                    type=str,
+                                    default="Gene",
+                                    help="Column headers for the columns containing the gene labels")
+    ap.add_argument("--outprefix",
+                    type=str,
+                    default="",
+                    help="Output prefix")
+    settings_group = ap.add_mutually_exclusive_group(required=True)
+    settings_group.add_argument("--reffile",
+                                help="sgRNA_reference_file")
+    settings_group.add_argument("--apply_w_hp",
+                                action='store_true',
+                                default=False,
+                                help="Apply with hp")
+    return ap
 
-Example: run_JACKS.py countfile replicatefile:rep_hdr:sample_hdr:ctrl_sample_or_hdr guidemappingfile:sgrna_hdr:gene_hdr
- --apply_w_hp --outputprefix prefix
-"""
-replicatefile_doc = """ 
-            "replicatefile:rep_hdr:sample_hdr:ctrl_sample_or_hdr"
-            
-            where replicatefile is a csv or tab delimited file mapping replicates to samples, 
-            rep_hdr and sample_hdr specify the column headers for the columns containing 
-            the replicate labels and sample labels respectively, and ctrl_sample specifies 
-            the name of the control sample
-            """
-guidemapping_doc = """
-                "guidemappingfile:sgrna_hdr:gene_hdr"
-                
-                where guidemappingfile is a csv or tab delimited file mapping guides to genes, 
-                sgrna_hdr and gene_hdr specify the column headers for the columns containing 
-                the guide labels and gene labels respectively.
-                """
-ap = argparse.ArgumentParser(description=run_jacks_doc)
-ap.add_argument("countfile",
-                help="Countfile: assumes sgrna label is always in the first column!")
-ap.add_argument("replicatefile",
-                help=replicatefile_doc)
-ap.add_argument("guidemappingfile",
-                help=guidemapping_doc)
-ap.add_argument("--outprefix",
-                type=str,
-                default="",
-                help="Output prefix")
-group = ap.add_mutually_exclusive_group(required=True)
-group.add_argument("--reffile",
-                   help="sgRNA_reference_file")
-group.add_argument("--apply_w_hp",
-                   action='store_true',
-                   default=False,
-                   help="Apply with hp")
 
 if __name__ == '__main__':
 
     LOG.setLevel(logging.WARNING)
-    args = ap.parse_args()
+    parser = get_run_jacks_parser()
+    args = parser.parse_args()
 
-    countfile = args.countfile
-    apply_w_hp = args.apply_w_hp
-    rep_toks = args.replicatefile.split(':')
-    if len(rep_toks) != 4:
-        raise Exception(
-            'Incorrect replicate file input: expecting "replicatefile:rep_hdr:sample_hdr:ctrl_sample_or_hdr" '
-            + replicatefile_doc)
-    replicatefile, rep_hdr, sample_hdr, ctrl_sample_or_hdr = rep_toks
-
-    guide_toks = args.guidemappingfile.split(':')
-    if len(guide_toks) != 3:
-        raise Exception(
-            'Incorrect guidemappingfile input: expecting "guidemappingfile:sgrna_hdr:gene_hdr" ' + guidemapping_doc)
-    guidemappingfile, sgrna_hdr, gene_hdr = guide_toks
-
+    ctrl_sample_or_hdr = args.ctrl_sample
     outprefix = args.outprefix
     if '/' in outprefix and not os.path.exists(os.path.dirname(outprefix)): os.makedirs(os.path.dirname(outprefix))
     outfile_w = outprefix + '_gene_JACKS_results.txt'
@@ -137,12 +134,12 @@ if __name__ == '__main__':
 
     # Load the specification of samples to include
     print('Loading sample specification')
-    sample_spec, ctrl_per_sample, ctrl_spec = createSampleSpec(countfile, replicatefile, rep_hdr, sample_hdr,
-                                                               ctrl_sample_or_hdr)
+    sample_spec, ctrl_per_sample, ctrl_spec = createSampleSpec(args.countfile, args.replicatefile, args.rep_hdr,
+                                                               args.sample_hdr, ctrl_sample_or_hdr)
 
     # Load the mappings from guides to genes
     print('Loading gene mappings')
-    gene_spec = createGeneSpec(guidemappingfile, sgrna_hdr, gene_hdr)
+    gene_spec = createGeneSpec(args.guidemappingfile, args.sgrna_hdr, args.gene_hdr)
 
     sgrna_reference_file = args.reffile
     if sgrna_reference_file:
@@ -178,7 +175,7 @@ if __name__ == '__main__':
         ctrldata = data[:, sample_ids.index(ctrl_sample_or_hdr), :]
         test_sample_idxs = [i for i, x in enumerate(sample_ids) if x != ctrl_sample_or_hdr]
         testdata = data[:, test_sample_idxs, :]
-    jacks_results = infer_JACKS(gene_index, testdata, ctrldata, apply_w_hp=apply_w_hp, fixed_x=x_reference)
+    jacks_results = infer_JACKS(gene_index, testdata, ctrldata, apply_w_hp=args.apply_w_hp, fixed_x=x_reference)
 
     # Write out the results
     print('Writing JACKS results')
