1d6196bfc0b4c796fd7bac467199caef26947ca4
Anton Khodak <anton.khodak@ukr.net>
2018-11-05 13:54:54 +0000

    Make arbitrary inputs work

--- server/app.py
+++ server/app.py
@@ -4,23 +4,38 @@ import uuid
 import wtforms
 from celery import Celery
 from flask import Flask, render_template, request, Blueprint, redirect, url_for
+from werkzeug.utils import secure_filename
+from wtforms.validators import DataRequired
 
 from jacks.jacks_io import loadJacksFullResultsFromPickle, getSortedGenes, getGeneWs, runJACKS, PICKLE_FILENAME, \
-    REP_HDR_DEFAULT, SAMPLE_HDR_DEFAULT, SGRNA_HDR_DEFAULT
+    REP_HDR_DEFAULT, SAMPLE_HDR_DEFAULT, SGRNA_HDR_DEFAULT, GENE_HDR_DEFAULT
 from plot_heatmap import plot_heatmap
 
-APP_ROOT = os.path.join(os.path.dirname(__file__), "..")
+APP_ROOT = os.path.dirname(os.path.dirname(__file__))
+ANALYSIS_FOLDER = os.path.join(APP_ROOT, 'results')
 CELERY_BROKER_URL = 'CELERY_BROKER_URL'
 CELERY_RESULT_BACKEND = 'CELERY_RESULT_BACKEND'
+MB = 1024 * 1024
 
 bp = Blueprint('jacks', __name__)
 app = Flask(__name__, template_folder="templates", static_url_path="/JACKS/static")
+app.config['MAX_CONTENT_LENGTH'] = 32 * MB
 app.config[CELERY_BROKER_URL] = os.getenv(CELERY_BROKER_URL, 'redis://localhost:6379/0')
 app.config[CELERY_RESULT_BACKEND] = os.getenv(CELERY_RESULT_BACKEND, 'redis://localhost:6379/0')
 
 celery = Celery(app.name, broker=app.config[CELERY_BROKER_URL], backend=app.config[CELERY_RESULT_BACKEND])
 celery.conf.update(app.config)
 
+YUSAV1_0 = 'yusav1.0'
+# YUSAV1_1 = 'yusav1.1'
+AVANA = 'avana'
+# BRUNELLO = 'brunello'
+# WHITEHEAD = 'whitehead'
+# TORONTO = 'toronto'
+REFERENCE_LIB_DICT = {
+    YUSAV1_0: os.path.join(APP_ROOT, 'reference_grna_efficacies', 'yusa_v10_grna_JACKS_results.txt'),
+    AVANA: os.path.join(APP_ROOT, 'reference_grna_efficacies', 'avana_grna_JACKS_results.txt'),
+}
 
 @celery.task()
 def send_to_jacks(countfile, replicatefile, guidemappingfile,
@@ -34,27 +49,28 @@ def send_to_jacks(countfile, replicatefile, guidemappingfile,
 
 
 def get_pickle_file(analysis_id):
-    return os.path.join(APP_ROOT, "results", analysis_id, PICKLE_FILENAME)
+    return os.path.join(ANALYSIS_FOLDER, analysis_id, PICKLE_FILENAME)
 
 
 class JacksForm(wtforms.Form):
-    raw_count_file = wtforms.FileField('Raw count file')
-    replicate_map_file = wtforms.FileField('Replicate map field')
-    header_replicates = wtforms.StringField('Header for replicates')
-    header_sample = wtforms.StringField('Header for sample')
-    ctrl_sample_name = wtforms.StringField('Name for a control sample')
-    grna_gene_map_file = wtforms.FileField('gRNA-Gene map file')
-    header_grna = wtforms.StringField('Header for gRNA id')
-    header_gene = wtforms.StringField('Header for Gene')
-    use_reference_lib = wtforms.RadioField('Use reference library', choices=[('ref', 'Use reference library'),
-                                                                             ('none', 'No reference')])
-    reference_lib = wtforms.SelectField('Reference library', choices=[("", 'None'),
-                                                                      ('yusav1.0', 'Yusa v1.0'),
-                                                                      ('yusav1.1', 'Yusa v1.1'),
-                                                                      ('avana', 'Avana'),
-                                                                      ('brunello', 'Brunello'),
-                                                                      ('whitehead', 'Whitehead'),
-                                                                      ('toronto', 'Toronto Knockout')])
+    raw_count_file = wtforms.FileField('Raw count file', validators=[DataRequired()])
+    replicate_map_file = wtforms.FileField('Replicate map file', validators=[DataRequired()])
+    header_replicates = wtforms.StringField('Header for replicates', default=REP_HDR_DEFAULT)
+    header_sample = wtforms.StringField('Header for sample', default=SAMPLE_HDR_DEFAULT)
+    ctrl_sample_name = wtforms.StringField('Name for a control sample', default="CTRL")
+    grna_gene_map_file = wtforms.FileField('gRNA-Gene map file', validators=[DataRequired()])
+    header_grna = wtforms.StringField('Header for gRNA id', default=SGRNA_HDR_DEFAULT)
+    header_gene = wtforms.StringField('Header for Gene', default=GENE_HDR_DEFAULT)
+    use_reference_lib = wtforms.BooleanField('Use reference library', default=False)
+    reference_lib = wtforms.SelectField('Reference library',
+                                        choices=[("", 'None'),
+                                                 (YUSAV1_0, 'Yusa v1.0'),
+                                                 # ('yusav1.1', 'Yusa v1.1'),
+                                                 (AVANA, 'Avana')],
+                                                 # ('brunello', 'Brunello'),
+                                                 # ('whitehead', 'Whitehead'),
+                                                 # ('toronto', 'Toronto Knockout')],
+                                                 default="")
     max_genes_display = wtforms.IntegerField('Max genes to display', default=20)
 
 
@@ -68,31 +84,33 @@ def start_analysis():
     template = "index.html"
     form = JacksForm(request.form)
     if request.method == 'POST':
-        raw_count_file = form.raw_count_file.data
-        replicate_map_file = form.replicate_map_file.data
+        analysis_id = str(uuid.uuid4()).replace("-", "")[:12] + "/"
+
+        def get_file(file):
+            return os.path.join(ANALYSIS_FOLDER, analysis_id, secure_filename(request.files[file].filename))
+
+        raw_count_file = get_file("raw_count_file")
+        replicate_map_file = get_file("replicate_map_file")
         header_replicates = form.header_replicates.data
         header_sample = form.header_sample.data
-        ctrl_sample_name = form.ctrl_sample_name.data
-        grna_gene_map_file = form.grna_gene_map_file.data
+        common_ctrl_sample = form.ctrl_sample_name.data
+        grna_gene_map_file = get_file("grna_gene_map_file")
         header_grna = form.header_grna.data
         header_gene = form.header_gene.data
         reference_lib = form.reference_lib.data
-        if not raw_count_file:
-            raw_count_file = grna_gene_map_file = os.path.join(APP_ROOT, "jacks/example-small/example_count_data.tab")
-            replicate_map_file = os.path.join(APP_ROOT, "jacks/example-small/example_repmap.tab")
-            reference_lib = None
-            header_grna = SGRNA_HDR_DEFAULT
-            header_gene = 'gene'
-            header_sample = SAMPLE_HDR_DEFAULT
-            header_replicates = REP_HDR_DEFAULT
-            common_ctrl_sample = "CTRL"
-
-        analysis_id = str(uuid.uuid4()).replace("-", "")[:12] + "/"
+        if reference_lib:
+            reference_lib = REFERENCE_LIB_DICT[reference_lib]
+        files = ['raw_count_file', 'replicate_map_file', 'grna_gene_map_file']
+        for file in files:
+            f = request.files[file]
+            file_path = get_file(file)
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+            f.save(file_path)
         send_to_jacks.delay(countfile=raw_count_file, replicatefile=replicate_map_file,
                             guidemappingfile=grna_gene_map_file,
                             rep_hdr=header_replicates, sample_hdr=header_sample, common_ctrl_sample=common_ctrl_sample,
                             sgrna_hdr=header_grna, gene_hdr=header_gene,
-                            outprefix=os.path.join(APP_ROOT, "results/") + analysis_id, reffile=reference_lib)
+                            outprefix=os.path.join(ANALYSIS_FOLDER, analysis_id), reffile=reference_lib)
         return render_template(template, form=form, analysis_id=analysis_id)
     return render_template(template, form=form)
 
